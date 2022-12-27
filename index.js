eedaf2faa959f446b9b8151b402d439bb817b4e6
Victor Gila Barreto <gilabarreto@gmail.com>
2022-12-27 13:03:24 -0700

    Create server with index, about and users route

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
-// console.log('Hello from Node.js')
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-// const { person } = require('./person')
 
-// console.log(person.name)
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile(path.join(__dirname, 'public', 'index.html'),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.end(content);
+      })
+  }
 
-// const { Person } = require('./person')
+  if (req.url === '/about') {
+    fs.readFile(path.join(__dirname, 'public', 'about.html'),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.end(content);
+      })
+  }
 
-// const person1 = new Person('Victor', 38);
+  if (req.url === '/api/users') {
+    const users = [
+      { name: 'Victor GB', age: 38 },
+      { name: 'Tyler Durden', age: 40 }
+    ];
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(users));
+  }
+});
 
-// person1.greeting();
+const PORT = process.env.PORT || 5000;
 
-const Logger = require('./logger')
-
-const logger =  new Logger();
-
-logger.on('message', (data) => console.log('Called Listener', data))
-
-logger.log('Hello World')
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
